4651e05b3bf889d44103caf3b0315402347f035e
unknown <inf132222@lab-44-12.cs.put.poznan.pl>
2018-10-29 14:20:59 +0100

    pierwsza zmiana

--- Program.java
+++ Program.java
@@ -1,7 +1,9 @@
+//dodatkowy komentarz
 //poczatek
 public class HelloWorld {
 //srodek
     public static void main(String[] args) {
+		//koniec
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World!");
     }
