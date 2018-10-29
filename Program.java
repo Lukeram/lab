fdedd73380f31a9024a013a31c22a18cd6c530da
unknown <inf132222@lab-44-12.cs.put.poznan.pl>
2018-10-29 14:17:38 +0100

    zmiany

--- Program.java
+++ Program.java
@@ -1,5 +1,6 @@
+//poczatek
 public class HelloWorld {
-
+//srodek
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World!");
